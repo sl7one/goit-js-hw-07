6c3795a6ee4dad57789f90e0166c432eabb76508
Slava <110696118+sl7one@users.noreply.github.com>
2022-10-23 21:49:47 +0300

    update tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  parentDom: document.querySelector('.gallery'),
+};
+
+// --------------создание строки и встраивание в дом элемент----------
+function makeMarkup(obj) {
+  return obj
+    .map(el => {
+      const { preview, original, description } = el;
+      return `<a class="gallery__link" href="#"> <img class="gallery__image lazyload" src="${preview}" data-src="${preview}" data-source="${original}" alt="${description}"/></a>`;
+    })
+    .join('');
+}
+const markup = makeMarkup(galleryItems);
+refs.parentDom.insertAdjacentHTML('beforeend', markup);
+let instanceNew;
+// -------------создание instance ----------------------------
+function setInstance(event) {
+  event.target.src = event.target.dataset.source;
+  const instance = basicLightbox.create(event.target.outerHTML);
+  return instance;
+}
+//-------------прослушиваем клик по галлерее------------------------------
+refs.parentDom.addEventListener('click', onClickGallery);
+function onClickGallery(event) {
+  if (event.target.classList.contains('gallery__image')) {
+    event.preventDefault();
+    instanceNew = setInstance(event);
+    instanceNew.show();
+    setListenerEsc();
+  }
+}
+//-------------прослушиваем клик по ESC------------------------------
+function onPressEsc(event) {
+  console.log('ловим клавишу', event.key);
+  if (event.key === 'Escape') {
+    instanceNew.close();
+    removeListenerEsc();
+  }
+}
+//-------------прослушиваем клик по документу, чтобы снять слушатель с клавиатуры------------
+document.addEventListener('click', onClic);
+function onClic(event) {
+  if (event.target.classList.contains('basicLightbox')) {
+    removeListenerEsc();
+  }
+}
+//-------------сеты для клавиатуры---------------------------------------
+function setListenerEsc() {
+  document.addEventListener('keydown', onPressEsc);
+}
+function removeListenerEsc() {
+  document.removeEventListener('keydown', onPressEsc);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refs = {
+  parentDom: document.querySelector('.gallery'),
+};
+// --------------создание строки и встраивание в дом элемент----------
+function makeMarkup(obj) {
+  return obj
+    .map(el => {
+      const { preview, original, description } = el;
+      return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a>`;
+    })
+    .join('');
+}
 
-console.log(galleryItems);
+const markup = makeMarkup(galleryItems);
+refs.parentDom.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  //   overlay: false, // СКРИПТ ПАДАЕТ НА 2ОМ ВЫЗОВЕ
+  captionsData: 'alt',
+  captionsDelay: 250,
+  //   captionPosition: 'right',
+  overlayOpacity: 0.5,
+  //   spinner: false,
+  //   nav: false,
+  //   navText: ['previous', 'next'],
+  //   caption: true,
+  //   closeText: false,
+  //   swipeClose: true,
+  //   showCounter: false,
+  //   animationSpeed: 1000,
+  //   animationSlide: false,
+  //   enableKeyboard: false,
+  //   loop: false,
+  //   swipeTolerance: 100,
+  //   widthRatio: 0.8,
+  //   disableRightClick: true,
+  //   additionalHtml: 'true',
+  //   throttleInterval: 2000,
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+lightbox.on('nextImageLoaded.simplelightbox', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+});
+
+lightbox.on('closed.simplelightbox', () => {
+  document.body.style.backgroundColor = 'white';
+});
